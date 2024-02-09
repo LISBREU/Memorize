0535aa476b9562117b9a3753d69218980fce5ba8
LISBREU <ilnurshabanov79@gmail.com>
2024-02-09 13:54:05 +0300

    start project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ilnur Shabanov on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
