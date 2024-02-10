ae5fa562b7f6bba7c126b6e708d814425aa0d80b
LISBREU <ilnurshabanov79@gmail.com>
2024-02-10 14:13:08 +0300

    some edit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -22,22 +22,23 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
+                        base.fill(.white)
                     .strokeBorder(lineWidth: 3)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        } .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
-
+ 
 
 
 
